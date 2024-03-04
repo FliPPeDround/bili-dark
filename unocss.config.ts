e9662ef443dd09737433ec76a13c3c2b52a1a456
flippedround <734243792@qq.com>
2024-03-04 20:23:21 +0800

    feat: home

--- unocss.config.ts
+++ unocss.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'unocss/vite'
 import { presetUno, transformerDirectives } from 'unocss'
 
 export default defineConfig({
+  shortcuts: [
+    ['btn', 'bg-dark-4! border-zinc-7! text-gray-4! hover:bg-zinc-7! hover:text-gray-3!'],
+  ],
   presets: [presetUno()],
   transformers: [
     transformerDirectives({
